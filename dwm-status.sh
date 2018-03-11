b27f4f7c2700229c7e8401d5183d8c06ba8fda1f
Julia <p1ngu1n0fd00m@gmail.com>
2018-03-11 11:55:36 +0100

    Add cleanup and dependency check
    Clean up the fifo after running the script. Check if all needed dependencies are installed.

--- dwm-status.sh
+++ dwm-status.sh
@@ -1,1 +1,31 @@
 #!/usr/bin/env bash
+
+trap cleanup TERM EXIT QUIT
+
+declare -r STATUS_FIFO="/tmp/status-fifo"
+
+function cleanup {
+	if [[ -e $STATUS_FIFO ]] ; then
+		rm -f $STATUS_FIFO
+	fi
+}
+
+function check_dependency {
+	for dep in "$@" ; do
+		if ! which "$dep" &> /dev/null ; then
+			echo "Missing $dep"
+			exit 1
+		fi
+	done
+}
+
+function reset_fifo {
+	if [[ -e $STATUS_FIFO ]] ; then
+		rm -f $STATUS_FIFO
+	fi
+	mkfifo $STATUS_FIFO
+}
+
+reset_fifo
+
+check_dependency clock date xsetroot mpc
